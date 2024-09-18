73842018bc6e82f9efcf2a55b084b97715b107b6
denikhairulanam <denikhairul201@gmaiol.com>
2024-09-18 09:14:07 +0700

    menambahkam

---
+++ PRAKTIKUM 2/index.php
@@ -0,0 +1,32 @@
+<?php
+include "orang.php";
+include "bankcustomers.php";
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class>praktikum 2</h1>
+
+    <div class="di">
+        <?php 
+           $nasabah = new bankCustomrs();
+           $nasabah->setcustomersName ("deni");
+           $nasabah->setAdres("mendalo, jambi");
+           $nasabah->setemail ("emely .goggle. com");
+           $nasabah->setcard ( "wadiah");
+           $nasabah->getacc(" platinum");
+
+           $nasabah ->insertCard();
+           $nasabah->enterPIN(123456);
+?>
+    </div>
+    
+</body>
+</html>
+<!--localhost/index.php?nama=james-->
+<!--http://uin jambi .ac.id /berita ?page=1 =1&search =kuliah&orderBy=id-->
---
+++ PRAKTIKUM 2/orang.php
@@ -0,0 +1,9 @@
+<?php
+
+class Orang{
+    public $nama ;
+
+    public function ucapkansalam(){
+        echo"assalamualaikum perkenalkan nama saya", $this->nama;
+    }
+}
